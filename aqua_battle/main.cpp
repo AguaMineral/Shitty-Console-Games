07b8bf93f6929d2576b9f8b9782c15f5259a85f6
AguaMineral <m.villa.lore@gmail.com>
2018-02-24 19:04:02 +0100

    Arreglo de bugs y añadiendo el checkPositioin

--- aqua_battle/main.cpp
+++ aqua_battle/main.cpp
@@ -4,8 +4,8 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define COLUMNS 10
-#define ROWS 10
+#define COLUMNS 11
+#define ROWS 11
 
 using namespace std;
 
@@ -22,28 +22,42 @@ public:
     {
         for(int i = 0; i < ROWS; i++)
             for (int k = 0; k < COLUMNS; k++)
+            {
+                if(i == 0 || i == 10){_arr[i][k] = '#';}
+                else if(k==0 || k == 10){_arr[i][k] == '#';}
                 _arr[i][k] = 'O';
+            }
+
+    }
+    //Funcion que resetea el tablero
+    void ResetTable()
+    {
+        Table();
     }
 
     //Función que actualiza el tablero con las tiradas de cada turno
-    void SetTable(Table& table, int x, int y, const string& s, char rotation)
+    void SetTable(Table& table, int x, int y, const string& ship, char rotation)
     {
         //Rotación horizontal
         if (rotation == 'H')
         {
+            CheckPos(table, x, y, ship, rotation);
             //bucle para introducir el barco en el tablero
             for ( int i = 0; i < 1; i++)
             {
-                for ( int j = 0; j < s.size(); j++)
-                    table._arr[x+i][y+j] = s[i];
+                for ( int j = 0; j < ship.size(); j++){
+
+                    table._arr[x+i][y+j] = ship[i];
+                }
             }
 
         }//Rotación vertical
         else if(rotation == 'V')
         {
+            CheckPos(table, x, y, ship, rotation);
             //bucle para introducir el barco en el tablero
-            for ( int i = 0; i < s.size(); i++)
-                table._arr[x+i][y] = s[i];
+            for ( int i = 0; i < ship.size(); i++)
+                table._arr[x+i][y] = ship[i];
         }
         else
             cout << "Error, formato de rotación no válido" << endl;
@@ -56,13 +70,44 @@ public:
             cout <<  i+1 << " ";
         for(int i = 0; i < ROWS; i++)
         {
-
             cout << endl;
             cout << "\t" << index[i] << " ";
             for(int j = 0; j < COLUMNS; j++)
                 cout << _arr[i][j] << " ";
         }
         cout << endl;
+    }
+
+    /////////////////// REGLAS DE POSICIONAMIENTO DE BARCOS /////////////////////
+
+    bool CheckPos(Table& table, int x, int y, const string& ship, char rot)
+    {
+        if ( rot == 'H')
+        {
+            cout << "h" << endl;
+            for ( int i = y; i < ship.size()+y; i++ )
+            {
+                    //Comprobar si se superponen- Comprueba si hay barco contiguo
+                if ( table._arr[x][i] == 'X' || table._arr[x-1][i] == 'X'|| table._arr[x+1][i] == 'X'|| table._arr[x][y-1] == 'X'|| table._arr[x][y+ship.size()] == 'X'
+                    || (ship.size()+y) > COLUMNS || (ship.size() - y) < 0)//->comprueba si se sale del tablero
+                {cout << "Posicion erronea, ya hay un barco" << endl; return false;}
+            }
+            cout << "Posicion correcta" << endl;
+            return true;
+        }
+        else if( rot == 'V')
+        {
+            //Comprobar si se superponen
+            cout << "v" << endl;
+            for ( int i = x; i < ship.size()+ x; i++)
+            {
+                if ( table._arr[i][y] == 'X' || table._arr[i+1][y+1] == 'X'|| table._arr[i+1][y-1] == 'X'|| table._arr[i-1][y] == 'X'|| table._arr[i+ship.size()][y] == 'X'
+                    || table._arr[x+ship.size()][y] != 'X' && table._arr[x+ship.size()][y] != 'O' )
+                {cout << "Posicion erronea, ya hay un barco" << endl; return false;}
+            }
+            cout << "Posicion correcta" << endl;
+            return true;
+        }
     }
 };
 
@@ -98,14 +143,22 @@ int main()
 
     ///////////////////// GAME LOOP ///////////////////////////////////////////
 
-    Table a;
+    //Table a;
 
     while(!ganador)
     {
+        Table a;
         char rot = 'H';
+        char rot2 = 'V';
+        int x = rand() % 10;
+        int y = rand() % 10;
+        int x2 = rand() % 10;
+        int y2 = rand() % 10;
         //se le pasa la instancia del tablero, las coordenadas donde quiere situarse el barco, el tipo de barco y la posición de este
-        a.SetTable(a, 5, 5, fragata, rot);
+  //      a.SetTable(a, 6, 7, fragata, rot);
+  //      a.SetTable(a, 7, 0, fragata, rot2);
         a.PrintTable();
+        a.ResetTable();
         cin.get();
     }
     return 0;
